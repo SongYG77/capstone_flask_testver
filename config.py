6639ce6224a4ce57806fa0a9d07bb611c0dae02b
SongYG <rjawoakstp@naver.com>
2021-07-20 01:30:28 +0900

    add db

--- config.py
+++
@@ -1,9 +0,0 @@
-db = {
-    'user'     : 'capstone',
-    'password' : 'capstones',
-    'host'     : 'capdb.c8wz24ghmr8c.us-east-2.rds.amazonaws.com',
-    'port'     : '3306',
-    'database' : 'capdb'
-}
-
-DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
